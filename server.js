14bde00825bf7def06e53ff8d0a79ab2b357fff7
Surya SIngh <144656857+EndlessGoodness@users.noreply.github.com>
2025-04-22 18:22:36 +0530

    Port Setup

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express=require('express');
 const ejs= require('ejs');
 const mongoose= require('mongoose');
 const Event=require('./models/event');
+require('dotenv').config();
 
 //database setup
-const dburl='mongodb+srv://faultyline007:V15h3NzORihOFCvc@cluster0.xlcuelf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose
-    .connect(dburl)
+    .connect(process.env.dburl)
     .then((result)=>{
         console.log('Connected to MongoDB');
         app.listen(5000,()=>{
@@ -18,10 +18,36 @@ mongoose
     });
 
 const app= express();
-app.get('/',(req,res)=>{
+/*app.get('/',(req,res)=>{
     res.send('Hello, World');
-});
+});*/
 app.listen(3000,()=>{
-    console.log('Server started on 3000');
+    console.log('App started on 3000');
 });
 app.set('view engine','ejs');
+
+//Handling form data
+app.post('/submit-event',(req,res)=>{
+    const event= new Event(req.body);
+    event.save()
+        .then((result)=>{
+            res.redirect('/');
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
+
+
+/*app.get('/', (req, res) => {
+    Event.find()
+      .then((result) => {
+        res.render('index', { title: 'All event', events: result })
+      })
+      .catch((err) => {
+        console.error(err); 
+    })
+  })*/
+  app.get('/create-event', (req, res) => {
+    res.render('form'); // Render the form.ejs file
+});
